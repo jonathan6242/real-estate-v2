0b97c20a89a5813a74eb386c5717c575c3506c52
Jonathan Wong <jonathanfwong@gmail.com>
2022-10-09 12:14:39 +1100

    testing

--- src/components/SidebarOne.jsx
+++ src/components/SidebarOne.jsx
@@ -11,7 +11,7 @@ function SidebarOne({ open, setOpen }) {
         onClick={() => setOpen(false)}
       ></div>
       <div className={`fixed left-0 inset-y-0 bg-white text-black z-30 duration-500 ease w-60
-      ${open ? 'translate-x-0' : '-translate-x-[200%]'}`}>
+      ${open ? 'translate-x-0 opacity-100' : '-translate-x-[200%] opacity-0'}`}>
         <div className="flex flex-col text-sm uppercase font-medium divide-y border-y">
           <div
             onClick={() => {
